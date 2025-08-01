531c09086d50d2846a201e647476193dc424a34f
Lucas Martins Vieira <lucasmartvieira03@outlook.com>
2025-07-31 21:09:16 -0300

    feat: add get todos handler

--- main.go
+++ main.go
@@ -1,5 +1,32 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Todo struct {
+	ID        string `json:"id,omitempty"`
+	Item      string `json:"item,omitempty"`
+	Completed bool   `json:"completed,omitempty"`
+}
+
+var todos = []Todo{
+	{ID: "1", Item: "Study Golang", Completed: false},
+	{ID: "2", Item: "Read Book", Completed: false},
+	{ID: "3", Item: "Work", Completed: false},
+}
+
+func getTodos(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, todos)
+}
+
 func main() {
+	router := gin.Default()
+
+	router.GET("/todos", getTodos)
+
+	router.Run() // listen and serve on 0.0.0.0:8080
 
 }
